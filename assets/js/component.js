e6273ce48c7a355d5c91a5d63c55c444314f5409
동민 <kebi3477@naver.com>
2020-07-28 14:57:45 +0000

    Use static files in node.js
    open server with express framwork

---
+++ assets/js/component.js
@@ -0,0 +1,3 @@
+var model = {
+    message: '123123'
+}
--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-const http = require("http");
-const fs = require("fs");
+//const http = require("http");
+//const fs = require("fs");
+const express = require('express');
+const app = express();
+const router = require('./router/main')(app);
 const port = 8081;
 
-const server = http.createServer((req, res) => {
-    fs.readFile('./index.html', (err, data) => {
-        if(err) {
-            throw err;
-        }
-        res.end(data);
-    })
+
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+//app.use(express.static('public'));
+
+const server = app.listen(port, () => { //(req, res) => {
+    console.log(`Express server has started on ${port}`);
+    // fs.readFile('./index.html', (err, data) => {
+    //     if(err) {
+    //         console.log(err);
+    //         ///throw err;
+    //     }
+    //     //res.end(data);
+    // })
 })
-server.listen(port);
-server.on('listening', () => {
-    console.log(`${port}번 포트에서 서버 대기 중입니다.`)
-})
+
+app.use('/assets',express.static('assets'));
+
+//server.listen(port);
+// server.on('listening', () => {
+//     console.log(`${port}번 포트에서 서버 대기 중입니다.`)
+// })
+
 server.on('error', err => {
     console.log(err);
 })
